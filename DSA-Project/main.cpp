ae6be28246f0309c7f587e73d3ecb26aa2eaad2a
S-DRosa <123402571+S-DRosa@users.noreply.github.com>
2023-04-10 19:30:23 -0400

    added menu options

--- DSA-Project/main.cpp
+++ DSA-Project/main.cpp
@@ -7,6 +7,95 @@
 void ReadFile(std::string file_name, std::vector<std::vector<double>> * image_data);
 int main(int argc, char* argv[]) {
     //load sparse matrices from files
+    if(argc < 3){
+        std::cout<<"You were missing files";
+    }
+    else if(argc > 4){
+        std::cout<<"Too many inputs";
+    }
+    else{
+        std::string input1_fn = argv[1];
+        std::string input2_fn = argv[2];
+        std::string output_fn = argv[3];
+        std::string choice = "-1";
+        bool run = true;
+        while(run) {
+            std::cout << "Choose a value from the menu" << std::endl;
+            std::cout << "1.)View Sparse Matrix" << std::endl;
+            std::cout << "2.)Add or Multiply Sparse Matrix" << std::endl;
+            std::cout << "3.)Exit" << std::endl;
+            std::cin >>choice;
+
+            //This if statement will output the different sparse matrices
+            if(choice=="1"){
+                choice ="0";
+                while(run) {
+                    std::cout << "Which Sparse Matrix do you want to view" << std::endl;
+                    std::cout << "1.)Matrix from file1: " << input1_fn << std::endl;
+                    std::cout << "2.)Matrix from file2: " << input2_fn << std::endl;
+                    std::cout << "3.)Both file1 and file2 matrices" << std::endl;
+                    std::cout << "4.)Current output matrix" << std::endl;
+                    std::cout << "5.)Go back to main menu" << std::endl;
+                    std::cin >> choice;
+
+                    //Will Show the matrix from input1_fn
+                    if (choice == "1") {}
+                    //will show the matrix from input2_fn
+                    else if (choice == "2") {}
+                    //will show both input1_fn and input2_fn matrices
+                    else if (choice == "3") {}
+                    //Will show the matrix that will be put into the output file
+                    else if (choice == "4") {}
+                    //input is not valid
+                    else if(choice != "5"){std::cout<<"Invalid input: try again"<<std::endl;}
+                    else{
+                        run =false;
+                    }
+                }
+                run = true;
+            }
+            //This choice will let the user either multiply or add the given matrices
+            else if(choice == "2"){
+                choice = "0";
+                while(run) {
+                    std::cout << "Which math action do you want performed" << std::endl;
+                    std::cout << "1.)Multiply matrices" << std::endl;
+                    std::cout << "2.)Add Matrices" << std::endl;
+                    std::cout << "3.)return to main menu" << std::endl;
+                    std::cin >> choice;
+
+                    //Will multiply the matrices
+                    if (choice == "1") {}
+                    //Will add the matrices
+                    else if (choice == "2") {}
+                    else if (choice != "3") {std::cout<<"Invalid option: try again"<<std::endl;}
+                    else{
+                        run = false;
+                    }
+                }
+                run = true;
+            }
+            //This choice will let the user exit the program and save the output matrix if they like
+            else if(choice == "3"){
+                while(run) {
+                    std::cout << "Do you want to save current output matrix: (y)es or (n)o?" << std::endl;
+                    std::cin >> choice;
+                    if (choice == "y") {
+                        run = false;
+                        std::cout << "Saving output matrix to: " << output_fn << std::endl;
+                    }
+                    else if (choice != "n") {
+                        std::cout << "Wrong input: Try again" << std::endl;
+                    }
+                    else{run= false;}
+                }
+
+            }
+            else{std::cout<<"Invalid option: try again"<<std::endl;}
+
+        }
+        std::cout<<"Thank you for using our program";
+    }
 
 
 }
